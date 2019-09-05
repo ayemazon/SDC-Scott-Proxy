d2dd8bd298459c48bb3047972e2d46c8a5c19c35
deadcode <josephsonsjoseph@gmail.com>
2019-09-04 20:39:40 -0700

    "Added newrelic"

--- server/index.js
+++ server/index.js
@@ -5,8 +5,13 @@ const cors = require('cors');
 const path = require('path');
 const url = require('url');
 
+const sparksPaths = ['/products', 'products/:productId'];
+const derricksPaths = ['/product/:id'];
+const scottsPaths = ['/product/pricing/:id'];
+const juansPaths = []
+
 const app = express();
-const PORT = 3333;
+const PORT = 3300;
 app.use(cors());
 
 app.use(morgan("dev"));
@@ -14,7 +19,10 @@ app.use(express.static(path.join(__dirname, "../", "public")));
 app.use('/static', express.static(path.join(__dirname, "../", "public")));
 app.use('/:id', express.static(path.join(__dirname, "../", "public")));
 
-app.use('/product/:id', proxy('http://localhost:3030'));
+app.use(scottsPaths, proxy('http://localhost:3030'));
+app.use(derricksPaths, proxy('http://localhost:3005'));
+app.use(sparksPaths, proxy('http://localhost:3000'));
+app.use(juansPaths, proxy('http://localhost:8080'));
 
 app.listen(PORT, () => {
   console.log(`visit http://localhost:${PORT}`);
